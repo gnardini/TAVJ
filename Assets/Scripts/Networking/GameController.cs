18cc952ea0378994c6b091c7481103d138ed5aa3
gnardini <gonzalo.nardini.dev@gmail.com>
2017-08-26 16:50:37 -0300

    Fix start game

--- Assets/Scripts/Networking/GameController.cs
+++ Assets/Scripts/Networking/GameController.cs
@@ -9,7 +9,7 @@ public class GameController : MonoBehaviour {
     public Transform autoAttackPrefab;
     public bool isServer;
 
-    private const string SERVER_HOST = "192.168.0.18"; // "181.26.156.227";
+    private const string SERVER_HOST = "181.26.156.227";//"192.168.0.18"; // "181.26.156.227";
     private const int PORT = 5500;
     private Channel _channel;
     private Dictionary<int, ServerPlayer> _players;
@@ -18,20 +18,20 @@ public class GameController : MonoBehaviour {
     private int _lastId = 0;
     private int _localId;
 
-	void Start () {
+    void Start () {
         if (isServer) {
             _channel = new Channel(PORT);
             _players = new Dictionary<int, ServerPlayer>();
         } else {
-			_channel = new Channel(PORT+1);
+            _channel = new Channel(PORT+1);
             _channel.AddConnection(SERVER_HOST, PORT);
             _clientPlayers = new Dictionary<int, Player>();
             SendByteable(new GameStartInput());
         }
         _autoAttacks = new Dictionary<int, AutoAttack>();
-	}
-	
-	void Update () {
+    }
+
+    void Update () {
         if (isServer) {
             Packet packet = _channel.getPacket();
             while (packet != null) {
@@ -48,12 +48,12 @@ public class GameController : MonoBehaviour {
                     _autoAttacks.Add(_lastId, autoAttack);
                     _channel.SendAll(new AutoAttackResponse(auto.GetId(), auto.GetTargetPosition()));
                     break;
-				case InputType.START_GAME:
-					_lastId++;
-					Vector3 startPosition = new Vector3 (2f, 1.2f, 0f);
-					ServerPlayer serverPlayer = createServerPlayer(new PlayerInfo(_lastId, startPosition));
-					_players.Add (_lastId, serverPlayer);
-					_channel.Send (new PlayerInfoBroadcast (_lastId, _players), packet.getAddress());
+                case InputType.START_GAME:
+                    _lastId++;
+                    Vector3 startPosition = new Vector3 (2f, 1.2f, 0f);
+                    ServerPlayer serverPlayer = createServerPlayer(new PlayerInfo(_lastId, startPosition));
+                    _players.Add (_lastId, serverPlayer);
+                    _channel.Send (new PlayerInfoBroadcast (_lastId, _players), packet.getAddress());
                     _channel.SendAllExcluding(new NewPlayerEvent(_lastId, startPosition), packet.getAddress());
                     break;
                 }
@@ -71,48 +71,50 @@ public class GameController : MonoBehaviour {
                 byte[] bytes = packet.getData();
                 ServerResponse response = ServerResponse.fromBytes(bytes);
                 switch (response.GetResponseType()) {
-				case ResponseType.POSITIONS: {
-						MovementResponse movementResponse = (MovementResponse)response;
-						if(_clientPlayers.ContainsKey(movementResponse.GetId ())){
-							_clientPlayers [movementResponse.GetId ()].MakeMovement (movementResponse.GetPosition ());
+                case ResponseType.POSITIONS: {
+                        MovementResponse movementResponse = (MovementResponse)response;
+                        if(_clientPlayers.ContainsKey(movementResponse.GetId ())){
+                            _clientPlayers [movementResponse.GetId ()].MakeMovement (movementResponse.GetPosition ());
                         } else if (_autoAttacks.ContainsKey(movementResponse.GetId())) {
                             _autoAttacks[movementResponse.GetId()].MoveTo(movementResponse.GetPosition());
                         }
-						break;
-					}
-				case ResponseType.AUTOATTACK: {
-						AutoAttackResponse auto = (AutoAttackResponse)response;
-						_clientPlayers [auto.GetId ()].SpawnAutoAttack (auto.GetPosition ());
-						break;
-					}
-				case ResponseType.NEW_PLAYER: {
-						NewPlayerEvent newPlayerEvent = (NewPlayerEvent)response;
-						PlayerInfo playerInfo = newPlayerEvent.GetPlayerInfo ();
-						Player player = createPlayer (playerInfo);
-						_clientPlayers.Add (playerInfo.GetId (), player);
-						break;
-					}
-				case ResponseType.PLAYER_INFO_BROADCAST: { 
-						PlayerInfoBroadcast playerInfoBroadcast = (PlayerInfoBroadcast)response;
-						_localId = playerInfoBroadcast.GetId ();
-						foreach (PlayerInfo playerInfo in playerInfoBroadcast.getPlayersInfo()) {
-							Player player = createPlayer (playerInfo);
-							if (playerInfo.GetId () == _localId) {
-								player.SetId (_localId);
-								player.SetMoveLocally (true);
-								player.SetGameController (this);
-								player.gameObject.transform.GetChild (0).gameObject.SetActive (true);
-							} 
-							_clientPlayers.Add (playerInfo.GetId(), player);
-						}
-						break;
-					}
+                        break;
+                    }
+                case ResponseType.AUTOATTACK: {
+                        AutoAttackResponse auto = (AutoAttackResponse)response;
+                        _clientPlayers [auto.GetId ()].SpawnAutoAttack (auto.GetPosition ());
+                        break;
+                    }
+                case ResponseType.NEW_PLAYER: {
+                        NewPlayerEvent newPlayerEvent = (NewPlayerEvent)response;
+                        PlayerInfo playerInfo = newPlayerEvent.GetPlayerInfo ();
+                        if (!_clientPlayers.ContainsKey(playerInfo.GetId())) {
+                            Player player = createPlayer (playerInfo);
+                            _clientPlayers.Add (playerInfo.GetId (), player);   
+                        }
+                        break;
+                    }
+                case ResponseType.PLAYER_INFO_BROADCAST: { 
+                        PlayerInfoBroadcast playerInfoBroadcast = (PlayerInfoBroadcast)response;
+                        _localId = playerInfoBroadcast.GetId ();
+                        foreach (PlayerInfo playerInfo in playerInfoBroadcast.getPlayersInfo()) {
+                            Player player = createPlayer (playerInfo);
+                            if (playerInfo.GetId () == _localId) {
+                                player.SetId (_localId);
+                                player.SetMoveLocally (true);
+                                player.SetGameController (this);
+                                player.gameObject.transform.GetChild (0).gameObject.SetActive (true);
+                            } 
+                            _clientPlayers.Add (playerInfo.GetId(), player);
+                        }
+                        break;
+                    }
                 }
 
                 packet = _channel.getPacket();
             }
         }
-	}
+    }
 
     public void SendByteable(Byteable byteable) {
         _channel.SendAll(byteable);
@@ -122,12 +124,12 @@ public class GameController : MonoBehaviour {
         _channel.Destroy();   
     }
 
-	private ServerPlayer createServerPlayer(PlayerInfo playerInfo){
-		return Instantiate(serverPlayerPrefab, playerInfo.GetPosition(), Quaternion.identity).gameObject.GetComponent<ServerPlayer>();
-	}
+    private ServerPlayer createServerPlayer(PlayerInfo playerInfo){
+        return Instantiate(serverPlayerPrefab, playerInfo.GetPosition(), Quaternion.identity).gameObject.GetComponent<ServerPlayer>();
+    }
 
-	private Player createPlayer(PlayerInfo playerInfo){
-		return Instantiate(clientPlayerPrefab, playerInfo.GetPosition(), Quaternion.identity).gameObject.GetComponent<Player>();
-	}
+    private Player createPlayer(PlayerInfo playerInfo){
+        return Instantiate(clientPlayerPrefab, playerInfo.GetPosition(), Quaternion.identity).gameObject.GetComponent<Player>();
+    }
 }
 
